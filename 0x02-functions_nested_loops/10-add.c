092fe1f8247e6a065d4ecf33d2eb49c980acaeab
Chibuzor <asolo.chibuzor@gmail.com>
2023-07-19 20:09:40 -0700

    10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two values
+ * @x: The first number to be checked
+ * @y: The second number to be checked
+ * Return: An integer
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
