ae234211bd8c82f94765ce5b9c68c3c36b276049
Chibuzor <asolo.chibuzor@gmail.com>
2023-07-19 18:43:46 -0700

    5-sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * Prints the sign of a number
+ * @y: The number to be checked
+ * Return: 1 if number is positive,  -1 if number is negative or else 0
+ */
+
+int print_sign(int y)
+
+{
+	if (y > 0)
+	{
+	_putchar(43);
+	return (1);
+	}
+	else if (y < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
