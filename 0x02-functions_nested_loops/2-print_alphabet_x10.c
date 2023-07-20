df38c012faa3c02d9d274868762773fcfe1d8c66
Chibuzor <asolo.chibuzor@gmail.com>
2023-07-19 17:42:38 -0700

    updated

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,14 +8,14 @@
 void print_alphabet_x10(void)
 {
 	char x;
-	
+
 	int y;
 
 	y = 0;
 
 	while (y < 10)
 	{
-		for (x = 'a'; x <='z'; x++)
+		for (x = 'a'; x <= 'z'; x++)
 		{
 			_putchar(x);
 		}
@@ -23,5 +23,4 @@ void print_alphabet_x10(void)
 		y++;
 	}
 
-}
-	
+}	
