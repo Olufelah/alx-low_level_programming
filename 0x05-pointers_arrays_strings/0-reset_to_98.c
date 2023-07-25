a356d11b8d19b28920513fc978e634072e835a07
Olufela <asolo.chibuzor@gmail.com>
2023-07-25 14:56:21 -0700

    All

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,7 +4,7 @@
  * reset_to_98 - a function that takes a pointer to an int as parameter
  * updates the pointed value to 98
  * @n: Start value
- * Return: n 
+ * Return: n
  */
 
 void reset_to_98(int *n)
---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * puts2 - function to print only one character out of two
+ * beginning with the first one
+ * @str: Input
+ * Return: print
+ */
+
+void puts2(char *str)
+{
+	int longi = 0;
+	int i = 0;
+	char *x = str;
+	int a;
+
+	while (*x != '\0')
+	{
+		x++;
+		longi++;
+	}
+	i = longi - 1;
+	for (a = 0; a <= i; a++)
+	{
+		if (a % 2 == 0)
+		{
+			_putchar(str[a]);
+		}
+	}
+	_puhtchar('\n')
+}
